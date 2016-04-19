74ef8c4c3013e10dd8482b058ac406d7b23ea483
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-04-19 21:56:46 +0200

    removed some old example code

--- src/store.js
+++ src/store.js
@@ -36,35 +36,6 @@ function store(state, action) {
             selectedTopic: action.topics[0],
             topics: action.topics,
         });
-    // case 'ADD_TODO':
-    //     const allTodos = state.allTodos.concat([{
-    //         task: action.task,
-    //         state: 'pending',
-    //     }]);
-    //     return Object.assign({}, state, {
-    //         allTodos,
-    //         todos: allTodos.filter(todo => todo.state === state.filter),
-    //     });
-    // case 'DONE_TODO':
-    //     const doneTodo = Object.assign({}, action.todo, {
-    //         state: 'done',
-    //     });
-    //
-    //     const updatedAllTodos = state.allTodos.map((todo) => {
-    //         return todo === action.todo ? doneTodo : todo;
-    //     });
-    //
-    //     return Object.assign({}, state, {
-    //         allTodos: updatedAllTodos,
-    //         todos: updatedAllTodos.filter(todo => todo.state === state.filter),
-    //     });
-    // case 'TOGGLE_STATE':
-    //     const filter = state.filter === 'pending' ? 'done' : 'pending';
-    //     console.log(filter);
-    //     return Object.assign({}, state, {
-    //         filter,
-    //         todos: state.allTodos.filter(todo => todo.state === filter),
-    //     });
     default:
         return state;
     }
