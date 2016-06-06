d87bc9a1873b781fb44c5ae0866d2ead72b2c7ec
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-06-06 21:27:05 +0200

    lesson 3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-document.write('<h1>hello</h1>');
+import ReactDom from 'react-dom';
+import React from 'react';
+
+ReactDom.render((
+    <h1
+        style={{
+            color: 'blue',
+        }}
+    >hello there</h1>
+), document.getElementById('app'));
