c6e33ab38630d401263119d3f7e931240bd989ac
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-06-07 16:43:52 +0200

    lesson-14: rendering list and list items with material-ui components

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import ListItem from 'material-ui/List/ListItem';
+import Card from 'material-ui/Card/Card';
 
 const Link = ({ link }) => {
     return (
-        <div>{link.url}: {link.description} ({link.voteCount})</div>
+        <Card
+            key={link.id}
+            style={{
+                marginBottom: 10,
+                padding: 10,
+            }}
+        >
+            <div
+                style={{
+                    display: 'flex',
+                }}
+            >
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                    }}
+                >
+
+                    <div
+                        style={{
+                            fontSize: 20,
+                            textAlign: 'center',
+                        }}
+                    >
+                    {link.voteCount}
+                    </div>
+
+                </div>
+                <ListItem
+                    primaryText={link.url}
+                    secondaryText={link.description}
+                />
+            </div>
+        </Card>
     );
 };
 
