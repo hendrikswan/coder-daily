4eaf3b1349281a8c5ce985b546211480467e32b3
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-06-07 10:35:44 +0200

    lesson-7: link props

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Link = () => {
+const Link = ({ link }) => {
     return (
-        <div>this is a link</div>
+        <div>{link.url}: {link.description}</div>
     );
+};
+
+Link.propTypes = {
+    link: React.PropTypes.shape({
+        url: React.PropTypes.string.isRequired,
+        description: React.PropTypes.string.isRequired,
+    }).isRequired,
 };
 
 export default Link;
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,8 +6,18 @@ const LinkList = () => {
         <div>
             <h4>this is the link list:</h4>
 
-            <Link />
-            <Link />
+            <Link
+                link={{
+                    url: 'http://google.co.za',
+                    description: 'the good \'ol search engine',
+                }}
+            />
+            <Link
+                link={{
+                    url: 'http://tagtree.io',
+                    description: 'my old site',
+                }}
+            />
         </div>
     );
 };
