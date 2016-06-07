7b3522042fa3631aad845f2627b3ebca6a3c054f
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-06-07 16:23:17 +0200

    lesson-13: introduced material ui and styled navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import List from 'material-ui/List/List';
 
-const Navigation = ({ topics, onTopicSelected }) => {
-    const topicNodes = topics.map(topic => (
-        <li
-            key={topic.id}
-            onClick={() => onTopicSelected({ topic })}
-        >
-            {topic.name}
-        </li>
-    ));
 
-    return (
-        <ul>
-            {topicNodes}
-        </ul>
-    );
-};
+class Navigation extends React.Component {
+    state = {
+        open: false,
+    };
+
+    toggle = () => this.setState({ open: !this.state.open });
+
+    render() {
+        const topicNodes = this.props.topics.map(topic => {
+            return (
+                <MenuItem
+                    linkButton={true}
+                    key={topic.id}
+                    onTouchTap={() => {
+                        this.props.onTopicSelected({ topic });
+                        this.setState({ open: false });
+                    }}
+                >
+                    {topic.name}
+                </MenuItem>
+            );
+        });
+
+        return (
+            <div>
+                <AppBar
+                    title="Coder daily"
+                    onTitleTouchTap={this.toggle}
+                    onLeftIconButtonTouchTap={this.toggle}
+                >
+                </AppBar>
+
+                <Drawer open={this.state.open}
+                    docked={false}
+                    onRequestChange={open => this.setState({ open })}
+                >
+                    <List>
+                        {topicNodes}
+                    </List>
+                </Drawer>
+            </div>
+        );
+    }
+}
 
 Navigation.propTypes = {
-    topics: React.PropTypes.arrayOf(
-        React.PropTypes.shape({
-            name: React.PropTypes.string.isRequired,
-        }),
-    ),
-    onTopicSelected: React.PropTypes.func.isRequired,
+    topics: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            id: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+    onTopicSelected: PropTypes.func.isRequired,
 };
 
 export default Navigation;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import App from './components/App';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+injectTapEventPlugin();
 
 ReactDom.render((
-    <App />
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <App />
+    </MuiThemeProvider>
 ), document.getElementById('app'));
