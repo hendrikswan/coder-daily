069297a569e1b49b763e3cada09601383c7bd42c
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-06-07 10:57:10 +0200

    lesson-8: generating link elements in LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,24 +2,30 @@ import React from 'react';
 import Link from './Link';
 
 const LinkList = () => {
+    const links = [{
+        id: 1,
+        url: 'http://google.co.za',
+        description: 'the good \'ol search engine',
+        voteCount: 10,
+    }, {
+        id: 2,
+        url: 'http://tagtree.io',
+        description: 'my old site',
+        voteCount: 3,
+    }];
+
+    const linkNodes = links.map(link => (
+        <Link
+            key={link.id}
+            link={link}
+        />
+    ));
+
     return (
         <div>
             <h4>this is the link list:</h4>
 
-            <Link
-                link={{
-                    url: 'http://google.co.za',
-                    description: 'the good \'ol search engine',
-                    voteCount: 10,
-                }}
-            />
-            <Link
-                link={{
-                    url: 'http://tagtree.io',
-                    description: 'my old site',
-                    voteCount: 3,
-                }}
-            />
+            {linkNodes}
         </div>
     );
 };
