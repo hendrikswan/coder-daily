d152e939342bd59877e0d5f455c7634f926e434d
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-06-08 23:04:21 +0200

    lesson-16: removed provider - hooked up manually

--- src/main.js
+++ src/main.js
@@ -4,16 +4,12 @@ import App from './components/App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import store from './store';
-import { Provider } from 'react-redux';
 
 
 injectTapEventPlugin();
 
 ReactDom.render((
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </MuiThemeProvider>
 ), document.getElementById('app'));
